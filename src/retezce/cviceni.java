a1391aa4b29f1659aaa95db67cfa66bbb16e5a6a
matej.breu <matej.breu@infis.cz>
2025-12-01 10:30:41 +0100

    2

---
+++ src/retezce/cviceni.java
@@ -0,0 +1,13 @@
+package retezce;
+
+public class cviceni {
+    public static void main(String[] args) {
+        String a = "x";
+        String b = "y";
+        String c = a+b;
+        String d = "xy";
+
+        if (c.equals(d)) System.out.println("ano");
+        else System.out.println("ne");
+    }
+}
